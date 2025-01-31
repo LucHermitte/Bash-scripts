2f49b656abd1872b1c81ffcd5c78073223914c19
Luc Hermitte <luc.hermitte@gmail.com>
2025-01-31 14:10:54 +0100

    ENH: [nm_n_grep] Ignore undefined symbols

--- nm_n_grep.sh
+++ nm_n_grep.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# Copyright Luc Hermitte 2006-2014, all rigths reserved
+# Copyright Luc Hermitte 2006-2025, all rigths reserved
 # Licence GPL v3
 
 usage() {
     echo "nm_n_grep.sh [--brief|-b] <library-name> <symbol-searched>"
     echo " Typical use:"
     echo "   find . \( -name '*.so' -o -name '*.a' \) -exec nm_n_grep.sh {} symbol \; -print"
-    echo "   ldd executable | sed 's#.*=>[      ]*##' | xargs -I {}  ~1/nm_n_grep.sh -b {} symbol"
+    echo "   ldd executable | sed 's#.*=>[      ]*\\([^ ]*\\).*#\\1#' | xargs -I {}  nm_n_grep.sh -b {} symbol"
 }
 
 case $1 in
@@ -20,22 +20,22 @@ regex=''
 
 while [ $# -gt 0 ] ; do
     case $1 in
-	-b|--brief)
-	    brief=1
-	    ;;
-	*)
-	    if [ x"$file" == x"" ] ; then
-		file=$1
-	    else
-		regex=$1
-	    fi
-	    ;;
+        -b|--brief)
+            brief=1
+            ;;
+        *)
+            if [ x"$file" == x"" ] ; then
+                file=$1
+            else
+                regex=$1
+            fi
+            ;;
     esac
     shift
 done
 
 if [ $brief -eq 1 ] ; then
-        (nm -C $file | grep $regex > /dev/null) && echo "$regex found in $file"
+    (nm -CU $file 2>/dev/null | grep $regex > /dev/null) && echo "$regex found in $file"
 else
-        nm -C $file | grep $regex
+    nm -CU $file 2>/dev/null | grep $regex
 fi
